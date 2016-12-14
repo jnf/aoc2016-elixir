9ac0cbdffaf18fdc2b665c4bc1cffec1db3487ce
jnf <jeremy.flores@gmail.com>
2016-12-14 07:45:35 -0800

    adds an async solution for day5, part 2

--- day5.2/lib/d5.2.ex
+++ day5.2/lib/d5.2.ex
@@ -3,6 +3,56 @@ defmodule D5 do
   @workers 25
 
   @doc ~S"""
+      # iex> D5.build2 "abc"
+      # "05ace8e3"
+
+      iex> D5.build2 "uqwqemis"
+      "694190cd"
+  """
+  def build2(str, start \\ 0, code \\ "--------")
+  def build2(str, start, code) do
+    new_code = batch2(str, start, code)
+    if String.contains?(new_code, "-") do
+      next_start = @workers * @size + start
+      build2(str, next_start, new_code)
+    else
+      new_code
+    end
+
+  end
+
+  defp batch2(str, start, code) do
+    0..@workers - 1
+      |> Enum.map(fn(i) -> Task.async(fn -> hash2(str, start + (i * @size)) end) end)
+      |> Enum.map(&Task.await/1)
+      |> List.flatten
+      |> Enum.reduce(code, fn({pos, val}, code) -> update_code(code, pos, val) end)
+  end
+
+  defp hash2(str, start) do
+    Enum.reduce start..start+@size, [], fn (n, acc) ->
+      <<a, b, pos, d>> = binary_part :crypto.hash(:md5, str <> to_string(n)), 0, 4
+
+      if a == 0 and b == 0 and pos <= 7 do
+        {val, _} = Base.encode16(<<d>>, case: :lower) |> String.next_codepoint
+        [{pos, val} | acc]
+      else
+        acc
+      end
+    end
+  end
+
+  defp update_code(code, pos, val) do
+    if String.at(code, pos) == "-", do: replace(code, pos, val), else: code
+  end
+
+  defp replace(code, pos, val) do
+    tail = 7 - pos
+    <<a::binary-size(pos), _b::binary-size(1), c::binary-size(tail)>> = code
+    a <> val <> c
+  end
+
+  @doc ~S"""
       # iex> D5.solve "abc"
       # "18f47a30"
 
@@ -38,3 +88,4 @@ defmodule D5 do
     end
   end
 end
+
